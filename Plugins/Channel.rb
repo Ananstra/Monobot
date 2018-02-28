835e9e022144f656d7bac21060e30e7af5c541e8
Dylan Abraham <abrahamd@cat.pdx.edu>
2018-02-28 00:04:48 -0800

    Add channel commands

---
+++ Plugins/Channel.rb
@@ -0,0 +1,24 @@
+require 'cinch'
+
+class Channel
+    include Cinch::Plugin
+
+    match /monojoin (#?\S+)( \S+)?/, method: :join
+    match /monopart (#?\S+)( \S+)?/, method: :part
+
+    def join(m,chan,key = nil)
+        return if not m.user.nick == "kimani"
+        chan = "#" + chan if (!chan.start_with?("#"))
+        key.lstrip! if (!key.nil?)
+        Channel(chan).join(key)
+        m.reply "Joining #{chan}"
+    end
+
+    def part(m,chan,msg = nil)
+        return if not m.user.nick == "kimani"
+        chan = "#" + chan if (!chan.start_with?("#"))
+        msg.lstrip! if (!msg.nil?)
+        m.reply "Leaving #{chan}"
+        Channel(chan).part(msg)
+    end
+end
