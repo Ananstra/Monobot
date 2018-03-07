84520e290ee8ecc73cbf6b56a665e91a8e8c9d9d
Dylan Abraham <abrahamd@cat.pdx.edu>
2018-03-06 22:50:32 -0800

    Add source command

---
+++ Plugins/Source.rb
@@ -0,0 +1,10 @@
+require "cinch"
+
+class Source
+    include Cinch::Plugin
+    :set prefix, "Monobot"
+    match /:?\W?source/
+    def execute(m)
+        m.reply "https://github.com/Ananstra/Monobot"
+    end
+end
