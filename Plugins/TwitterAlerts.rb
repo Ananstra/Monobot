9d835b060ebb19a437847aec7d911ddef0903fd3
Dylan Abraham <abrahamd@cat.pdx.edu>
2018-03-19 23:23:17 -0700

    Fixify twitter?

--- Plugins/TwitterAlerts.rb
+++ Plugins/TwitterAlerts.rb
@@ -28,21 +28,22 @@ class TwitterAlerts
             end
             return true
         end
-
-        client.filter(follow: "22589282,4194486134,5652522,1965093320") do |object|
-            if object.is_a?(Twitter::Tweet)
-                debug "User #{object.user.screen_name} sends #{object.text}. Reply #{object.reply?} Retweet #{object.retweet?} Quote #{object.quote?}"
-                if object.text.start_with?("PSU Alert") && is_originator?(object,"Portland_State")
-                    Channel("#robots").send object.text
-                end
-                if is_originator?(object,"trimet")
-                    Channel("#robots").send "Trimet alert: " + object.text
-                end
-                if is_originator?(object,"Ananstra22")
-                    Channel("#bots").send "Kimani test message: " + object.text
-                end
-                if is_originator?(object,"cat_alerts")
-                    Channel("#robots").send "CAT Alert:" + object.text
+        loop do
+            client.filter(follow: "22589282,4194486134,5652522,1965093320") do |object|
+                if object.is_a?(Twitter::Tweet)
+                    debug "User #{object.user.screen_name} sends #{object.text}. Reply #{object.reply?} Retweet #{object.retweet?} Quote #{object.quote?}"
+                    if object.text.start_with?("PSU Alert") && is_originator?(object,"Portland_State")
+                        Channel("#robots").send object.text
+                    end
+                    if is_originator?(object,"trimet")
+                        Channel("#robots").send "Trimet alert: " + object.text
+                    end
+                    if is_originator?(object,"Ananstra22")
+                        Channel("#bots").send "Kimani test message: " + object.text
+                    end
+                    if is_originator?(object,"cat_alerts")
+                        Channel("#robots").send "CAT Alert:" + object.text
+                    end
                 end
             end
         end
